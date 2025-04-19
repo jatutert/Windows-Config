8758105c5786f7b4d5a420814efee46a0f68d011
John Tutert <jatutert@gmail.com>
2025-04-19 12:00:44 +0200

    Bugfixes

--- Powershell/VM-OOBE-Winget-Install-APPS-V001.ps1
+++ Powershell/VM-OOBE-Winget-Install-APPS-V001.ps1
@@ -4,28 +4,33 @@
 #	19 april 2025
 #
 
+cmd.exe /c "winget source update"
+
+#	Error
+#	0x8a15000f : Data required by the source is missing
+
 #	Microsoft C Runtime
-winget install --id Microsoft.VCRedist.2010.x64 --architecture x64 --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.VCRedist.2012.x64 --architecture x64 --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.VCRedist.2013.x64 --architecture x64 --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.VCRedist.2015+.x64 --architecture x64 --accept-package-agreements --accept-source-agreements
+cmd.exe /c "winget install --id Microsoft.VCRedist.2010.x64 --architecture x64 --accept-package-agreements --accept-source-agreements"
+cmd.exe /c "winget install --id Microsoft.VCRedist.2012.x64 --architecture x64 --accept-package-agreements --accept-source-agreements"
+cmd.exe /c "winget install --id Microsoft.VCRedist.2013.x64 --architecture x64 --accept-package-agreements --accept-source-agreements"
+cmd.exe /c "winget install --id Microsoft.VCRedist.2015+.x64 --architecture x64 --accept-package-agreements --accept-source-agreements"
 
 #	Microsoft DotNET Runtime 
-winget install --id Microsoft.DotNet.DesktopRuntime.6 --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.DotNet.DesktopRuntime.7 --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.DotNet.DesktopRuntime.8 --accept-package-agreements --accept-source-agreements
+cmd.exe /c "winget install --id Microsoft.DotNet.DesktopRuntime.6 --accept-package-agreements --accept-source-agreements"
+cmd.exe /c "winget install --id Microsoft.DotNet.DesktopRuntime.7 --accept-package-agreements --accept-source-agreements"
+cmd.exe /c "winget install --id Microsoft.DotNet.DesktopRuntime.8 --accept-package-agreements --accept-source-agreements"
 
 #	Microsoft Powershell 7.5
-winget install Microsoft.Powershell --accept-package-agreements --accept-source-agreements
+cmd.exe /c "winget install Microsoft.Powershell --accept-package-agreements --accept-source-agreements"
 
 #	Microsoft Windows Terminal 
-winget install Microsoft.WindowsTerminal --accept-package-agreements --accept-source-agreements
+cmd.exe /c "winget install Microsoft.WindowsTerminal --accept-package-agreements --accept-source-agreements"
 
 #	GNU Nano
-winget install GNU.Nano --accept-package-agreements --accept-source-agreements
+cmd.exe /c "winget install GNU.Nano --accept-package-agreements --accept-source-agreements"
 
 #	Wget2
-winget install GNU.Wget2 --accept-package-agreements --accept-source-agreements
+cmd.exe /c "winget install GNU.Wget2 --accept-package-agreements --accept-source-agreements"
 
 #	CURL
-winget install cURL.cURL --accept-package-agreements --accept-source-agreements
+cmd.exe /c "winget install cURL.cURL --accept-package-agreements --accept-source-agreements"
