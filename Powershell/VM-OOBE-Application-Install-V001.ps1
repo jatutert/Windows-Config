ca6dc0fbc756ab0eb41464d7d731f80307990453
John Tutert <jatutert@gmail.com>
2025-04-21 00:36:35 +0200

    Bugfixes

--- Powershell/VM-OOBE-Application-Install-V001.ps1
+++ Powershell/VM-OOBE-Application-Install-V001.ps1
@@ -15,8 +15,24 @@
 #	WERKT NIET
 #	NO PASS
 #
-
 #	cmd.exe /s "c:\Users\Vagrant\Downloads\PowerShell-7.5.0-win-x64.MSI /quiet"
+
+#	###########################################################33
+#	Nieuwe poging
+#	21 april
+#	NOT TESTED 
+
+# Verkrijg de huidige gebruikersnaam
+$username = [System.Environment]::UserName
+
+# Define the path where the installer will be downloaded
+$installerPath = "C:\Users\$username\Downloads\\PowerShell-7.5.0-win-x64.msi"
+
+# Define the arguments for the MSI installer
+$msiArguments = "/i `"$installerPath`" /quiet"
+
+# Install PowerShell 7
+Start-Process msiexec.exe -ArgumentList $msiArguments -Wait
 
 #
 #	That is all Folks
