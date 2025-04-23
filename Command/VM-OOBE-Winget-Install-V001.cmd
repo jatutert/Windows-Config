274b2e276553449fbad6a0125e34e9d0999735b0
John Tutert <jatutert@gmail.com>
2025-04-23 21:07:01 +0200

    Download and install

--- Command/VM-OOBE-Winget-Install-V001.cmd
+++ Command/VM-OOBE-Winget-Install-V001.cmd
@@ -1,11 +1,58 @@
-:: 
-@winget install --id Microsoft.VCRedist.2010.x64 --architecture x64 --force --silent --accept-package-agreements --accept-source -agreements
-@winget install --id Microsoft.VCRedist.2012.x64 --architecture x64 --force --silent --accept-package-agreements --accept-source-agreements
-@winget install --id Microsoft.VCRedist.2013.x64 --architecture x64 --force --silent --accept-package-agreements --accept-source-agreements
-@winget install --id Microsoft.VCRedist.2015+.x64 --architecture x64 --force --silent --accept-package-agreements --accept-source-agreements
 ::
-@winget install --id Microsoft.DotNet.DesktopRuntime.6 --force --silent --accept-package-agreements --accept-source-agreements
-@winget install --id Microsoft.DotNet.DesktopRuntime.7 --force --silent --accept-package-agreements --accept-source-agreements
-@winget install --id Microsoft.DotNet.DesktopRuntime.8 --force --silent --accept-package-agreements --accept-source-agreements
+::	Microsoft Windows Out of the Box Expierence (OOBE) Script
+::	Created by John Tutert for TutSOFT
 ::
-@winget install --id Microsoft.WindowsTerminal --force --silent --accept-package-agreements --accept-source-agreements
+::	Versie 1
+::	23 april 2025
+::
+::	:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::	RUNTIMES
+::
+::	:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::	Microsoft C++ Runtime
+::
+@winget install --id Microsoft.VCRedist.2010.x64 --architecture x64 --accept-package-agreements --accept-source -agreements
+@winget install --id Microsoft.VCRedist.2012.x64 --architecture x64 --accept-package-agreements --accept-source-agreements
+@winget install --id Microsoft.VCRedist.2013.x64 --architecture x64 --accept-package-agreements --accept-source-agreements
+@winget install --id Microsoft.VCRedist.2015+.x64 --architecture x64 --accept-package-agreements --accept-source-agreements
+::
+::	Microsoft DotNET Runtime
+::
+@winget install --id Microsoft.DotNet.DesktopRuntime.6 --accept-package-agreements --accept-source-agreements
+@winget install --id Microsoft.DotNet.DesktopRuntime.7 --accept-package-agreements --accept-source-agreements
+@winget install --id Microsoft.DotNet.DesktopRuntime.8 --accept-package-agreements --accept-source-agreements
+::
+::	:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::	Windows Components
+::
+::	:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::	Microsoft Windows Terminal
+@winget install --id Microsoft.WindowsTerminal --accept-package-agreements --accept-source-agreements
+::
+::	:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::	Windows Utilities
+::
+::	:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::	SysInternals RAMMap
+::
+@winget install --id Microsoft.Sysinternals.RAMMap --accept-package-agreements --accept-source-agreements
+::
+::	:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::	Applications
+::
+::	:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+::	NotePAD++
+::
+@winget install --id Notepad++.Notepad++ --accept-package-agreements --accept-source-agreements
+::
+::
+::	Thats all folks
+::
---
+++ Powershell/VM-Download-V002.ps1
@@ -0,0 +1,123 @@
+#
+#
+#	Virtual Machine File Download Script
+#	Created by John Tutert for TutSOFT
+#
+#
+#	Version 2
+#	18 april 2025
+#
+#
+
+
+#	#################################################################################################################
+#
+#	Applicaties
+#
+#	#################################################################################################################
+
+
+#
+#	#################################################################################################################
+#	# RAMMAP
+#	#################################################################################################################
+#
+#	https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/Powershell/VM-OOBE-Config-SSH-V001.ps1
+#
+# Definieer de URL van het bestand dat je wilt downloaden
+$rmsurl = "https://download.sysinternals.com/files/RAMMap.zip"
+# Verkrijg de huidige gebruikersnaam
+$username = [System.Environment]::UserName
+# Definieer het pad waar het bestand moet worden opgeslagen
+$rmzipdestinationPath = "C:\Users\$username\Downloads\RAMMap.zip"
+$rmdestinationPath = "C:\Users\$username\Desktop"
+# Zorg ervoor dat de directory bestaat
+$directory = [System.IO.Path]::GetDirectoryName($rmzipdestinationPath)
+if (-not (Test-Path -Path $directory)) {
+    New-Item -ItemType Directory -Path $directory -Force
+}
+#
+# Download het bestand en sla het op in de gedefinieerde directory
+Invoke-WebRequest -Uri $rmsurl -OutFile $rmzipdestinationPath
+
+# Uitpakken ZIP-Bestand naar folder Desktop 
+Microsoft.PowerShell.Archive\Expand-Archive -LiteralPath $rmzipdestinationPath -DestinationPath $rmdestinationPath
+
+
+#
+#	#################################################################################################################
+#	# Powershell 7.5 
+#	#################################################################################################################
+#
+#	x64
+#	https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/PowerShell-7.5.0-win-x64.exe
+#	https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/PowerShell-7.5.0-win-x64.msi
+#
+# Definieer de URL van het bestand dat je wilt downloaden
+$ps75url = "https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/PowerShell-7.5.0-win-x64.msi"
+# Verkrijg de huidige gebruikersnaam
+$username = [System.Environment]::UserName
+# Definieer het pad waar het bestand moet worden opgeslagen
+$ps75destinationPath = "C:\Users\$username\Downloads\PowerShell-7.5.0-win-x64.msi"
+# Zorg ervoor dat de directory bestaat
+$directory = [System.IO.Path]::GetDirectoryName($ps75destinationPath)
+if (-not (Test-Path -Path $directory)) {
+    New-Item -ItemType Directory -Path $directory -Force
+}
+#
+# Download het bestand en sla het op in de gedefinieerde directory
+Invoke-WebRequest -Uri $ps75url -OutFile $ps75destinationPath
+
+
+#
+#	#################################################################################################################
+#	# NotePAD++
+#	#################################################################################################################
+#
+# Definieer de URL van het bestand dat je wilt downloaden
+$nppurl = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.7.9/npp.8.7.9.Installer.x64.exe"
+# Verkrijg de huidige gebruikersnaam
+$username = [System.Environment]::UserName
+# Definieer het pad waar het bestand moet worden opgeslagen
+$nppdestinationPath = "C:\Users\$username\Downloads\npp.8.7.9.Installer.x64.exe"
+# Zorg ervoor dat de directory bestaat
+$directory = [System.IO.Path]::GetDirectoryName($nppdestinationPath)
+if (-not (Test-Path -Path $directory)) {
+    New-Item -ItemType Directory -Path $directory -Force
+}
+#
+# Download het bestand en sla het op in de gedefinieerde directory
+Invoke-WebRequest -Uri $nppurl -OutFile $nppdestinationPath
+
+
+#	#################################################################################################################
+#
+#	Scripts
+#
+#	#################################################################################################################
+
+
+#
+#	#################################################################################################################
+#	# Winget APP Windows Command (CMD) Installer vanaf GitHUB JT
+#	#################################################################################################################
+#
+# Definieer de URL van het bestand dat je wilt downloaden
+$wgiurl = "https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/Command/VM-OOBE-Winget-Install-V001.cmd"
+# Verkrijg de huidige gebruikersnaam
+$username = [System.Environment]::UserName
+# Definieer het pad waar het bestand moet worden opgeslagen
+$wgidestinationPath = "C:\Users\$username\Downloads\VM-OOBE-Winget-Install-V001.cmd"
+# Zorg ervoor dat de directory bestaat
+$directory = [System.IO.Path]::GetDirectoryName($wgidestinationPath)
+if (-not (Test-Path -Path $directory)) {
+    New-Item -ItemType Directory -Path $directory -Force
+}
+#
+# Download het bestand en sla het op in de gedefinieerde directory
+Invoke-WebRequest -Uri $wgiurl -OutFile $wgidestinationPath
+
+
+#
+# Thats all folks
+#
