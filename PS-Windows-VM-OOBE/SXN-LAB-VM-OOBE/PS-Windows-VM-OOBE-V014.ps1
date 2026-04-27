e0b2432e9fe0e7965552a5076cb952cdef80b44d
John Tutert <jatutert@gmail.com>
2026-04-27 19:42:42 +0200

    Vmware Tools Cur

--- PS-Windows-VM-OOBE/SXN-LAB-VM-OOBE/PS-Windows-VM-OOBE-V014.ps1
+++ PS-Windows-VM-OOBE/SXN-LAB-VM-OOBE/PS-Windows-VM-OOBE-V014.ps1
@@ -29,6 +29,12 @@ Clear-Host
 #
 #
 #   #######################################################################
+#   Bepalen Windows Desktop of Windows Server 
+#   #######################################################################
+#
+$osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
+#
+#   #######################################################################
 #   TutSOFT Windows Register Sleutel aanmaken
 #   #######################################################################
 #
@@ -45,47 +51,23 @@ Write-Host "[2/15] Script directories aanmaken ..." -ForegroundColor Yellow
 #
 mkdir "$env:USERPROFILE\.TutSOFT" -Force | Out-Null
 mkdir "$env:USERPROFILE\.TutSOFT\OOBE" -Force | Out-Null
-mkdir "$env:USERPROFILE\.TutSOFT\OOBE\SSH" -Force | Out-Null
-mkdir "$env:USERPROFILE\.TutSOFT\OOBE\winservices" -Force | Out-Null
-mkdir "$env:USERPROFILE\.TutSOFT\OOBE\winrm" -Force | Out-Null
-mkdir "$env:USERPROFILE\.TutSOFT\OOBE\winget" -Force | Out-Null
-mkdir "$env:USERPROFILE\Downloads\vmware" -Force | Out-Null
-mkdir "$env:USERPROFILE\Desktop\ADDS" -Force | Out-Null
 #
 New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "OOBE Directories" -Value 1 -PropertyType DWord -Force | Out-Null
 #
 #
 #   #######################################################################
-#   Windows SSH Server
+#   Alle Windows 
 #   #######################################################################
 #
 #
-Write-Host "[3/15] SSH Server installeren en configureren..." -ForegroundColor Yellow
-#
-$OOBE_Config_SSH = "$env:USERPROFILE\.TutSOFT\OOBE\SSH\VM-OOBE-Config-SSH-Latest.ps1"
-#
-Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-VM-OOBE/Windows-Remote-SSH/VM-OOBE-Config-SSH-Latest.ps1 -OutFile $OOBE_Config_SSH
-#
-$timeout = 0
-while (!(Test-Path $OOBE_Config_SSH) -and $timeout -lt 10) {
-    Start-Sleep -Seconds 1
-    $timeout++
-}
-# 
-if ((Get-Item $OOBE_Config_SSH ).Length -gt 0) {
-    & $OOBE_Config_SSH 
-}
-#
-New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "Windows SSH" -Value 1 -PropertyType DWord -Force | Out-Null
-#
-#
-#   #######################################################################
+#   ###########################################
 #   Windows Services
-#   #######################################################################
+#   ###########################################
 #
 #
-Write-Host "[4/15] Windows Services stoppen en uitschakelen..." -ForegroundColor Yellow
+Write-Host "Windows Services stoppen en uitschakelen..." -ForegroundColor Yellow
 #
+mkdir "$env:USERPROFILE\.TutSOFT\OOBE\winservices" -Force | Out-Null
 $OOBE_Config_Services = "$env:USERPROFILE\.TutSOFT\OOBE\winservices\VM-OOBE-Config-Services-Latest.ps1"
 #
 Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-VM-OOBE/Windows-Services-Config/VM-OOBE-Config-Services-Latest.ps1 -OutFile $OOBE_Config_Services
@@ -103,37 +85,14 @@ if ((Get-Item $OOBE_Config_Services ).Length -gt 0) {
 New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "Windows Services" -Value 1 -PropertyType DWord -Force | Out-Null
 #
 #
-#   #######################################################################
-#   Windows WinRM
-#   #######################################################################
-#
-#
-Write-Host "[4/15] Windows WinRM service activeren en configureren..." -ForegroundColor Yellow
-#
-$OOBE_Config_WinRM = "$env:USERPROFILE\.TutSOFT\OOBE\winrm\VM-OOBE-Windows-WinRM-Config-Latest.ps1"
-#
-Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-VM-OOBE/Windows-WinRM-Config/VM-OOBE-Windows-WinRM-Config-Latest.ps1 -OutFile $OOBE_Config_WinRM
-#
-$timeout = 0
-while (!(Test-Path $OOBE_Config_WinRM) -and $timeout -lt 10) {
-    Start-Sleep -Seconds 1
-    $timeout++
-}
-# 
-if ((Get-Item $OOBE_Config_WinRM ).Length -gt 0) {
-    & $OOBE_Config_WinRM
-}
-#
-New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "Windows WinRM" -Value 1 -PropertyType DWord -Force | Out-Null
-#
-#
-#   #######################################################################
+#   ###########################################
 #   WinGET Deel 1 van 2 
-#   #######################################################################
+#   ###########################################
 #
 #
-Write-Host "[5/15] WinGET Installeren ..." -ForegroundColor Yellow
+Write-Host "WinGET Installeren ..." -ForegroundColor Yellow
 #
+mkdir "$env:USERPROFILE\.TutSOFT\OOBE\winget" -Force | Out-Null
 $OOBE_WinGET_Install = "$env:USERPROFILE\.TutSOFT\OOBE\winget\VM-OOBE-winget-install-Latest.ps1"
 #
 Invoke-WebRequest -URI https://raw.githubusercontent.com/asheroto/winget-install/master/winget-install.ps1 -OutFile $OOBE_WinGET_Install
@@ -144,10 +103,93 @@ while (!(Test-Path $OOBE_WinGET_Install) -and $timeout -lt 10) {
 }
 # 
 if ((Get-Item $OOBE_WinGET_Install ).Length -gt 0) {
-    & $OOBE_WinGET_Install
+    & $OOBE_WinGET_Install -Force
 }
 #
 New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "Winget Install" -Value 1 -PropertyType DWord -Force | Out-Null
+#
+#
+#   #######################################################################
+#   Windows Server
+#   #######################################################################
+#
+if ($osInfo.ProductType -ne 1) {
+    #
+    #
+    #   ###########################################
+    #   Windows SSH Server
+    #   ###########################################
+    #
+    #
+    Write-Host "SSH Server installeren en configureren..." -ForegroundColor Yellow
+    #
+    mkdir "$env:USERPROFILE\.TutSOFT\OOBE\SSH" -Force | Out-Null
+    $OOBE_Config_SSH = "$env:USERPROFILE\.TutSOFT\OOBE\SSH\VM-OOBE-Config-SSH-Latest.ps1"
+    #
+    Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-VM-OOBE/Windows-Remote-SSH/VM-OOBE-Config-SSH-Latest.ps1 -OutFile $OOBE_Config_SSH
+    #
+    $timeout = 0
+    while (!(Test-Path $OOBE_Config_SSH) -and $timeout -lt 10) {
+        Start-Sleep -Seconds 1
+        $timeout++
+    }
+    # 
+    if ((Get-Item $OOBE_Config_SSH ).Length -gt 0) {
+        & $OOBE_Config_SSH 
+    }
+    #
+    New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "Windows SSH" -Value 1 -PropertyType DWord -Force | Out-Null
+    #
+    #
+    #   ###########################################
+    #   Windows WinRM
+    #   ###########################################
+    #
+    #
+    Write-Host "Windows WinRM service activeren en configureren..." -ForegroundColor Yellow
+    #
+    mkdir "$env:USERPROFILE\.TutSOFT\OOBE\winrm" -Force | Out-Null
+    $OOBE_Config_WinRM = "$env:USERPROFILE\.TutSOFT\OOBE\winrm\VM-OOBE-Windows-WinRM-Config-Latest.ps1"
+    #
+    Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-VM-OOBE/Windows-WinRM-Config/VM-OOBE-Windows-WinRM-Config-Latest.ps1 -OutFile $OOBE_Config_WinRM
+    #
+    $timeout = 0
+    while (!(Test-Path $OOBE_Config_WinRM) -and $timeout -lt 10) {
+        Start-Sleep -Seconds 1
+        $timeout++
+    }
+    # 
+    if ((Get-Item $OOBE_Config_WinRM ).Length -gt 0) {
+        & $OOBE_Config_WinRM
+    }
+    #
+    New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "Windows WinRM" -Value 1 -PropertyType DWord -Force | Out-Null
+    #
+    #
+    #   ###########################################
+    #   TutSOFT Configuratie scripts en bestanden 
+    #   ###########################################
+    #
+    #
+    Write-Host "[14/15] Configuratie scripts downloaden ..." -ForegroundColor Yellow
+    #
+    #   ##################
+    #   TutSOFT SF ADDS
+    #   #################
+    #
+    #
+    Write-Host 'Active Directory Domain Services Scripts en bestanden'
+    #
+    mkdir "$env:USERPROFILE\Desktop\ADDS" -Force | Out-Null
+    Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-Active-Directory/WS22-AD-DC-Install.ps1 -OutFile "$env:USERPROFILE\Desktop\ADDS\WS22-AD-DC-Install.ps1"
+    Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-Active-Directory/WS22-AD-DC-Promote.ps1 -OutFile "$env:USERPROFILE\Desktop\ADDS\WS22-AD-DC-Promote.ps1"
+    Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-Active-Directory/WS22-AD-DC-Import-Users.ps1 -OutFile "$env:USERPROFILE\Desktop\ADDS\WS22-AD-DC-Import-Users.ps1"
+    Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-Active-Directory/ad_gebruikers.csv -OutFile "$env:USERPROFILE\Desktop\ADDS\ad_gebruikers.csv"
+    #
+    New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "ADDS Scripts" -Value 1 -PropertyType DWord -Force | Out-Null
+    #
+    #
+} 
 #
 #
 #   #######################################################################
@@ -155,19 +197,36 @@ New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "Winget Install" -Value 1 
 #   #######################################################################
 #
 #
-Write-Host "[6/15] VMware Tools installeren ..." -ForegroundColor Yellow
+$registryPath = "HKLM:\SOFTWARE\VMware, Inc.\VMware Tools"
 #
-$VMWARE_Tools_Installer = "$env:USERPROFILE\Downloads\vmware\VMware-tools-13.0.10-25056151-x64.exe"
-#
-Write-Host 'Downloading'
-#
-Invoke-WebRequest -URI https://packages.vmware.com/tools/releases/13.0.10/x64/VMware-tools-13.0.10-25056151-x64.exe -OutFile $VMWARE_Tools_Installer
-#
-Write-Host 'Installing'
-# 
-Start-Process -FilePath $VMWARE_Tools_Installer -ArgumentList "/s", "/v/qn", "REBOOT=ReallySuppress", "EULAS_AGREED=1" -Wait
-#
-New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "VMware Tools" -Value 1 -PropertyType DWord -Force | Out-Null
+if (-not (Test-Path -Path $registryPath)) {
+    #
+    Write-Host "[6/15] VMware Tools installeren ..." -ForegroundColor Yellow
+    #
+    mkdir "$env:USERPROFILE\Downloads\vmware" -Force | Out-Null
+    $VMWARE_Tools_Installer = "$env:USERPROFILE\Downloads\vmware\VMware-tools-13.0.10-25056151-x64.exe"
+    #
+    Write-Host 'Downloading VMware Tools'
+    #
+    Invoke-WebRequest -URI https://packages.vmware.com/tools/releases/13.0.10/x64/VMware-tools-13.0.10-25056151-x64.exe -OutFile $VMWARE_Tools_Installer
+    #
+    $timeout = 0
+    while (!(Test-Path $$VMWARE_Tools_Installer) -and $timeout -lt 10) {
+        Start-Sleep -Seconds 1
+        $timeout++
+    }
+    #
+    if ((Get-Item $VMWARE_Tools_Installer ).Length -gt 0) {
+        Write-Host 'Installing VMware Tools'
+        Start-Process -FilePath $VMWARE_Tools_Installer -ArgumentList "/s", "/v/qn", "REBOOT=ReallySuppress", "EULAS_AGREED=1" -Wait
+    }
+    #
+    New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "VMware Tools" -Value 1 -PropertyType DWord -Force | Out-Null
+    #
+}
+else {
+        Write-Host "VMware Tools zijn al aanwezig. Geen actie nodig." -ForegroundColor Gray
+}
 #
 #
 #   #######################################################################
@@ -252,30 +311,7 @@ Write-Host "[13/15] [WinGet] UniGetUI installeren ..." -ForegroundColor Yellow
 winget Install Devolutions.UniGetUI
 #
 New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "UniGetUI Install" -Value 1 -PropertyType DWord -Force | Out-Null
-#
-#
-#   #######################################################################
-#   TutSOFT Configuratie scripts en bestanden 
-#   #######################################################################
-#
-#
-Write-Host "[14/15] Configuratie scripts downloaden ..." -ForegroundColor Yellow
-#
-#   ###########################################
-#   TutSOFT SF Active Directory Domain Services
-#   ###########################################
-#
-#
-Write-Host 'Active Directory Domain Services Scripts en bestanden'
-#
-Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-Active-Directory/WS22-AD-DC-Install.ps1 -OutFile "$env:USERPROFILE\Desktop\ADDS\WS22-AD-DC-Install.ps1"
-Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-Active-Directory/WS22-AD-DC-Promote.ps1 -OutFile "$env:USERPROFILE\Desktop\ADDS\WS22-AD-DC-Promote.ps1"
-Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-Active-Directory/WS22-AD-DC-Import-Users.ps1 -OutFile "$env:USERPROFILE\Desktop\ADDS\WS22-AD-DC-Import-Users.ps1"
-Invoke-WebRequest -URI https://raw.githubusercontent.com/jatutert/Windows-Config/refs/heads/main/PS-Windows-Active-Directory/ad_gebruikers.csv -OutFile "$env:USERPROFILE\Desktop\ADDS\ad_gebruikers.csv"
-#
-New-ItemProperty -Path "HKCU:\Software\TutSOFT" -Name "ADDS Scripts" -Value 1 -PropertyType DWord -Force | Out-Null
-#
-#
+
 #   ###########################################
 #   x
 #   ###########################################
